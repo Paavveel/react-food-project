7e07e11ce4c8396eaec6f74d9415f59e51708fc0
Pavel T <pa.tomilin@gmail.com>
2021-11-15 23:43:40 +0300

    Создает базовые компоненты, добавляет Router v6

---
+++ src/api.js
@@ -0,0 +1,16 @@
+import { API_URL } from './config';
+
+const getMealById = async (id) => {
+  const res = await fetch(`${API_URL}lookup.php?i=${id}`);
+  return await res.json();
+};
+const getAllCategories = async () => {
+  const res = await fetch(`${API_URL}categories.php`);
+  return await res.json();
+};
+const getFilteredCategory = async (catName) => {
+  const res = await fetch(`${API_URL}filter.php?c=${catName}`);
+  return await res.json();
+};
+
+export { getMealById, getAllCategories, getFilteredCategory };
---
+++ src/components/CategoryItem.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function CategoryItem({ idCategory }) {
+  return <div className='card'>{idCategory}</div>;
+}
+
+export default CategoryItem;
